cd045022b2fff45db123550c4103d70d523b874c
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-09-18 16:15:08 +0100

    Ticket 4

--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,15 +1,15 @@
 function getGradeFromPoints(points) {
-  if ((points) => 90) {
-    return "A";
-  } else if (Points >= 70) {
-    return "B";
-  } else if (pointss >= 50) {
-    return "C";
-  } else if (poInts >= 40) {
-    return "D";
+  if (points >= 90) { 
+      return "A";
+  } else if (points >= 70) {
+      return "B";
+  } else if (points >= 50) {
+      return "C";
+  } else if (points >= 40) {
+      return "D";
   }
   return "E";
 }
 
-const grade = getGradeFromPoints(90);
-console.log(Grade, "should be A");
+const grade = getGradeFromPoints(100);
+console.log(grade, "should be A");
