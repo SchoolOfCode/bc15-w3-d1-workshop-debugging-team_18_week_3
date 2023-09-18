868673254689834c80bff0f149ff1335f4582974
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-09-18 16:00:31 +0100

    ticket3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,13 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+  for (let i = 0; i < number.toString().length; i++) {
+    let digit = number.toString()[i];
+    console.log(digit);
+    if (digit === "9") {
       count++;
     }
-  }
-
-  return count;
+  } return count;
 }
+
+console.log(countNumberOfNinesInNumber(9999));
